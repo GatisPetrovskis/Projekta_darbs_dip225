42f9b13b34fffecb4fe5414e0b0792176a61218d
Gatis Petrovskis <144628545+GatisPetrovskis@users.noreply.github.com>
2024-01-18 15:57:18 +0000

    7_commit

--- PROJEKTA_DARBS_231RDB019.py
+++ PROJEKTA_DARBS_231RDB019.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import datetime
 import time
 import pandas as pd
@@ -11,56 +12,82 @@ driver = webdriver.Chrome(service=service)
 datums = datetime.datetime.now()
 diena = datums.day
 menesis = datums.month
+saite = f"https://www.rtu.lv/lv/sports/sporta-nodarbibas/pieteikties-nodarbibam?sport_type_id=&sport_lesson_id=&sport_trainer_id=&sport_address_id=&gender=&master_type=0&calendar_date=2024-0{menesis}-{diena}&calendar_view=agendaWeek#d=2024-0{menesis}-{diena}&v=agendaWeek"
 
-url = f"https://www.rtu.lv/lv/sports/sporta-nodarbibas/pieteikties-nodarbibam?sport_type_id=&sport_lesson_id=&sport_trainer_id=&sport_address_id=&gender=&master_type=0&calendar_date=2024-0{menesis}-{diena}&calendar_view=agendaWeek#d=2024-0{menesis}-{diena}&v=agendaWeek"
-driver.get(url)
-time.sleep(3)
-sikfaili = driver.find_element(By.CLASS_NAME, "uce-disable")
-sikfaili.click()
-poga = driver.find_element(By.XPATH, '//*[@id="unibit_modal_popup"]/div/div/div/div/div[1]')
-poga.click()
-time.sleep(1)
+def lietotaja_ievade():
+    while True:
+        ievade = input("Ievadiet sportu: ")
+        if (not ievade):
+            print("Ievadiet vertību: ")
+        else:
+            break 
+    return ievade
+sports = lietotaja_ievade()
 
-linki = []
-find = driver.find_elements(By.TAG_NAME, "a")
-for x in find:
-    value = x.get_attribute("innerText")
-    if "Volejbols" in value and "RTU darbiniekiem" not in value:
-        linki.append(x.get_attribute("href"))
+def saites(sports, url):
+    driver.get(url)
+    time.sleep(3)
+    try:
+        sikfaili = driver.find_element(By.CLASS_NAME, "uce-disable")
+        sikfaili.click()
+        poga = driver.find_element(By.XPATH, '//*[@id="unibit_modal_popup"]/div/div/div/div/div[1]')
+        poga.click()
+        time.sleep(1)
+    except NoSuchElementException:
+        pass
 
-nakama_nedela = driver.find_elements(By.TAG_NAME, "button")
-for nakama in nakama_nedela:
-    if  "fc-next-button" in nakama.get_attribute("class"):
-        nakama.click()
-        break
-time.sleep(1)
+    linki = []
+    find = driver.find_elements(By.TAG_NAME, "a")
+    for x in find:
+        value = x.get_attribute("innerText")
+        if f"{sports}" in value and "RTU darbiniekiem" not in value:
+            linki.append(x.get_attribute("href"))
 
-find_2 = driver.find_elements(By.TAG_NAME, "a")
-for x in find_2:
-    value = x.get_attribute("innerText")
-    if "Basketbols" in value and "RTU darbiniekiem" not in value:
-        linki.append(x.get_attribute("href"))
-        
-tabulas = []
-for elem in linki:
-    driver.get(elem)
-    time.sleep(2)
-    final = []
-    tabula = driver.find_element(By.TAG_NAME, "tbody").get_attribute("innerText")
-    splited = tabula.rstrip().replace("\t", "").split("\n")
-    for x in splited:
-        vertiba = x.split(":", 1)
-        for y in vertiba:
-            final.append(y)
+    nakama_nedela = driver.find_elements(By.TAG_NAME, "button")
+    for nakama in nakama_nedela:
+        if  "fc-next-button" in nakama.get_attribute("class"):
+            nakama.click()
+            break
+    time.sleep(1)
 
-    vardnica = {}
-    for i in range(0, len(final), 2):
-        vardnica[final[i]] = [final[i + 1]]
-    if int(vardnica.get("Laiks")[0][0:2]) >= diena and "Atcelts" not in vardnica:
-        jauns = pd.DataFrame.from_dict(vardnica)
-        tabulas.append(jauns)
+    find_2 = driver.find_elements(By.TAG_NAME, "a")
+    for x in find_2:
+        value = x.get_attribute("innerText")
+        if f"{sports}" in value and "RTU darbiniekiem" not in value:
+            linki.append(x.get_attribute("href"))
+    return linki
 
-df = pd.concat(tabulas, ignore_index=True)
-df.index = df.index + 1
-df.to_excel("Volejbols.xlsx")
-print(df)
+def iegust_tabulas(linki):
+    tabulas = []
+    for elem in linki:
+        driver.get(elem)
+        time.sleep(1)
+        final = []
+        tabula = driver.find_element(By.TAG_NAME, "tbody").get_attribute("innerText")
+        splited = tabula.rstrip().replace("\t", "").split("\n")
+        for x in splited:
+            vertiba = x.split(":", 1)
+            for y in vertiba:
+                final.append(y)
+        vardnica = {}
+        for i in range(0, len(final), 2):
+            vardnica[final[i]] = [final[i + 1]]
+        if int(vardnica.get("Laiks")[0][0:2]) >= diena and "Atcelts" not in vardnica:
+            jauns = pd.DataFrame.from_dict(vardnica)
+            tabulas.append(jauns)
+    return tabulas
+
+def excel_datne(tabulas, sports):
+    df = pd.concat(tabulas, ignore_index=True)
+    df.index = df.index + 1
+    if 'Dalībnieku skaits' in df.columns and df["Dalībnieku skaits"].isnull().values.any() == False:
+        df['Dalībnieku skaits'] = df['Dalībnieku skaits'].astype('int')
+    if 'Maksimālais dalībnieku skaits' in df.columns and df["Maksimālais dalībnieku skaits"].isnull().values.any() == False:
+        df['Maksimālais dalībnieku skaits'] = df['Maksimālais dalībnieku skaits'].astype('int')
+    if 'Minimālais dalībnieku skaits' in df.columns and df["Minimālais dalībnieku skaits"].isnull().values.any() == False:
+        df['Minimālais dalībnieku skaits'] = df['Minimālais dalībnieku skaits'].astype('int')
+    df.to_excel(f"{sports}.xlsx")
+
+majaslapas = saites(sports, saite)
+tabulas = iegust_tabulas(majaslapas)
+excel_datne(tabulas, sports)
