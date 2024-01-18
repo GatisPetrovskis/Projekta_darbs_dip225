78da44a5a2c87cc5ebcebbc960dd23c88c24e44b
Gatis Petrovskis <144628545+GatisPetrovskis@users.noreply.github.com>
2024-01-18 00:29:13 +0000

    3_commit

--- PROJEKTA_DARBS_231RDB019.py
+++ PROJEKTA_DARBS_231RDB019.py
@@ -13,4 +13,20 @@ menesis = datums.month
 
 url = f"https://www.rtu.lv/lv/sports/sporta-nodarbibas/pieteikties-nodarbibam?sport_type_id=&sport_lesson_id=&sport_trainer_id=&sport_address_id=&gender=&master_type=0&calendar_date=2024-0{menesis}-{diena}&calendar_view=agendaWeek#d=2024-0{menesis}-{diena}&v=agendaWeek"
 driver.get(url)
-time.sleep(5)
+time.sleep(3)
+sikfaili = driver.find_element(By.XPATH, '/html/body/div[16]/div/div/div/div/div[2]/a[1]')
+sikfaili.click()
+time.sleep(1)
+
+linki = []
+find = driver.find_elements(By.TAG_NAME, "a")
+for x in find:
+    value = x.get_attribute("innerText")
+    if "Volejbols" in value:
+        linki.append(x.get_attribute("href"))
+
+for elem in linki:
+    driver.get(elem)
+    time.sleep(3)
+    adrese = driver.find_element(By.XPATH, "/html/body/div[14]/div/div/div[2]/div[1]/div/table/tbody/tr[5]/td/a").get_attribute("innerText")
+    print(adrese)
